ca5fd418b097ef8e20ea5ba01709ca8901a6343c
Kisuke710 <117828065+Kisuke710@users.noreply.github.com>
2022-11-09 13:36:51 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['201098906252'] //ur owner number
-global.ownername = "𝑬𝑳𝑮𝑨𝒁𝑨𝑹 𝑨𝑳𝑾𝑨𝒁𝑬𝑹🐼🍷 ٫" //ur owner name
-global.ytname = "YT: Elgazar" //ur yt chanel name
-global.socialm = "GitHub: Elgazar" //ur github or insta name
-global.location = "Egypt, Cairo, Aizawl" //ur location
+global.vcardowner = ['0679510006'] //ur owner number
+global.ownername = "𝑼𝒓𝒂𝒉𝒂𝒓𝒂 𝒌𝒊𝒔𝒖𝒌𝒆 ᝰ ٫" //ur owner name
+global.ytname = "YT:" //ur yt chanel name
+global.socialm = "ziyad_710" //ur github or insta name
+global.location = "" //ur location
 
 //bot bomdy 
-global.owner = ['201098906252']
-global.ownertag = '201098906252' //ur tag number
-global.botname = '𝑩𝑶𝑻 𝑬𝑳𝑮𝑨𝒁𝑨𝑹🐼🍷 ٫' //ur bot name
-global.linkz = "https://youtube.com/channel/UCxVaIay8BccgBtsofagA6_g" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtube.com/channel/UCxVaIay8BccgBtsofagA6_g" //ur website to be displayed
+global.owner = ['0679510006']
+global.ownertag = '0679510006' //ur tag number
+global.botname = '𝑼𝒓𝒂𝒉𝒂𝒓𝒂 𝑩𝒐𝒕 ᝰ ٫' //ur bot name
+global.linkz = "" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
 global.botscript = 'https://chat.whatsapp.com/CMp1e9NHmHU5K5sCM2mmKn' //script link
-global.reactmoji = "🌚" //ur menu react emoji
-global.themeemoji = "🌞" //ur theme emoji
-global.packname = "⍣ الجزار الوزير يروحي ⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
+global.reactmoji = "☕" //ur menu react emoji
+global.themeemoji = "🌊" //ur theme emoji
+global.packname = "𝑼𝒓𝒂𝒉𝒂𝒓𝒂 𝒌𝒊𝒔𝒖𝒌𝒆 ᝰ" //ur sticker watermark packname
+global.author = "⍣0679510006" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['201098906252'] //ur premium numbers
+global.premium = ['0679510006'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
@@ -105,15 +105,15 @@ global.antitags = true
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
-    success: 'تم يروحي ✓',
-    admin: 'هذه الميزه للرجاله المشرفين,استرجل وجرب مره تانيه!! ',
-    botAdmin: 'يجب أن يكون البوت مسؤولاً أولاً!',
-    owner: 'هذه الميزة للمالك فقط!',
-    group: 'لميزة تستخدم فقط للمجموعات!',
+    success: 'تم ✓',
+    admin: 'this feature is only for admin !!',
+    botAdmin: 'im not admin in this group ',
+    owner: 'this feature is only for owner',
+    group: 'ميزة تستخدم فقط للمجموعات!',
     private: 'الميزه المستخدمة فقط للدردشة الخاصة!',
-    bot: 'هذه الميزة مخصصة للبوت فقط',
-    wait: 'استني شويه...',
-    linkm: 'أين الرابط?',
+    bot: 'this feature is only for the bot',
+    wait: 'please wait...',
+    linkm: 'where is the link ??',
     error: 'ايرور!!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     ban: 'لقد تم حظرك من قبل المالك ، إذا كنت تريد أن تكون غير محظور ، مالك الدردشة.',
